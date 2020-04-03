18b307fe8fcc42802b05a98b1ded07afb8bc2172
jpoyard <jpoy77@yahoo.com>
2020-04-04 00:54:39 +0200

    create stats viewer

--- src/app/ocean-of-code.component.style.ts
+++ src/app/ocean-of-code.component.style.ts
@@ -2,7 +2,21 @@ export const STYLE = `
 .container {
     display: flex;
     flex: 1 1 auto;
+    align-items: stretch;
     padding: 20px;
+}
+.toolbar {
+    width: 180px;
+    padding: 10px;
+}
+.buttons {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.middle-buttons {
+    display: flex;
+    flex-direction: column;
 }
 .draw-zone {
     flex: 1 1 auto;
--- src/app/ocean-of-code.component.ts
+++ src/app/ocean-of-code.component.ts
@@ -1,29 +1,140 @@
 import {STYLE} from "./ocean-of-code.component.style";
 import {Grid} from "./services/grid.class";
 import {GRID_PROPERTIES} from "./GRID_PROPERTIES";
+import {Cell, CellTypeEnum} from "./services/cell.class";
+import {DirectionEnum} from "./services/submarine.class";
 
 export class OceanOfCodeComponent extends HTMLElement {
+    public static readonly MARGE = 6;
+    private readonly TEXT_COLOR = "#000";
+    private readonly SURFACE_COLOR = "#fffff";
     private readonly LINE_COLOR = "#c8e0ff";
     private readonly SEA_COLOR = "#569dfa";
-    private readonly ISLAND_COLOR = "#06e941";
+    private readonly ISLAND_COLOR = "#FFC107";
     private shadow: ShadowRoot;
     private canvas: HTMLCanvasElement;
     private canvasCtx: CanvasRenderingContext2D;
     private cellSize: number;
     private grid: Grid;
+    private stats: Array<{ cell: Cell, stat: number }>;
 
     constructor() {
         super();
-        this.grid = new Grid(GRID_PROPERTIES.width, GRID_PROPERTIES.height, GRID_PROPERTIES.grid.split(''));
+        const tmpGrid = GRID_PROPERTIES.grid.replace(/\n|\r/gm, '').split('');
+        this.grid = new Grid(GRID_PROPERTIES.width, GRID_PROPERTIES.height, tmpGrid);
+        this.stats = this.grid.cells.map(cell => ({cell, stat: cell.type === CellTypeEnum.SEA ? 0 : -1}));
         this.init();
         window.addEventListener('resize', () => this.resize());
         this.resize();
+
+        window.onkeydown = (event: KeyboardEvent) => {
+         console.info(event.key)
+            switch (event.key) {
+                case "Delete":
+                    this.stats = this.grid.cells.map(cell => ({cell, stat: cell.type === CellTypeEnum.SEA ? 0 : -1}));
+                    this.draw();
+                    break;
+                case "ArrowUp":
+                    this.goto(DirectionEnum.NORTH);
+                    break;
+                case "ArrowDown":
+                    this.goto(DirectionEnum.SOUTH);
+                    break;
+                case "ArrowLeft":
+                    this.goto(DirectionEnum.WEST);
+                    break;
+                case "ArrowRight":
+                    this.goto(DirectionEnum.EST);
+                    break;
+            }
+        }
+    }
+
+    public goto(direction: DirectionEnum): void {
+        const max = this.stats.reduce((max, cur) => Math.max(max, cur.stat), 0);
+        const candidates = this.stats.filter(stat => stat.stat === max);
+        switch (direction) {
+            case DirectionEnum.NORTH:
+                candidates.forEach(stat => {
+                    const index = this.grid.getIndex({x: stat.cell.coordinate.x, y: stat.cell.coordinate.y+1});
+                    if (candidates.find(c => c.cell.index === index) && this.grid.isIndexValid(index) && this.grid.isAvailableCell(this.grid.getCell(index))) {
+                        stat.stat++;
+                    }
+                    return stat;
+                });
+                break;
+            case DirectionEnum.EST:
+                candidates.forEach(stat => {
+                    const index = this.grid.getIndex({x: stat.cell.coordinate.x + 1, y: stat.cell.coordinate.y});
+                    if (candidates.find(c => c.cell.index === index) && this.grid.isIndexValid(index) && this.grid.isAvailableCell(this.grid.getCell(index))) {
+                        stat.stat++;
+                    }
+                    return stat;
+                });
+                break;
+            case DirectionEnum.SOUTH:
+                candidates.forEach(stat => {
+                    const index = this.grid.getIndex({x: stat.cell.coordinate.x, y: stat.cell.coordinate.y + 1});
+                    if (candidates.find(c => c.cell.index === index) && this.grid.isIndexValid(index) && this.grid.isAvailableCell(this.grid.getCell(index))) {
+                        stat.stat++;
+                    }
+                    return stat;
+                });
+                break;
+            case DirectionEnum.WEST:
+                candidates.forEach(stat => {
+                    const index = this.grid.getIndex({x: stat.cell.coordinate.x - 1, y: stat.cell.coordinate.y});
+                    if (candidates.find(c => c.cell.index === index) && this.grid.isIndexValid(index) && this.grid.isAvailableCell(this.grid.getCell(index))) {
+                        stat.stat++;
+                    }
+                    return stat;
+                });
+                break;
+            default:
+                break
+        }
+        this.draw();
     }
 
     private init() {
         this.shadow = this.attachShadow({mode: 'open'});
         const container = document.createElement('div');
         container.classList.add('container');
+
+        const toolbar = document.createElement('div');
+        toolbar.classList.add('toolbar');
+
+        const buttons = document.createElement('div');
+        buttons.classList.add('buttons');
+
+        const leftButton = document.createElement('button');
+        leftButton.textContent = 'West';
+        leftButton.onclick = () => this.goto(DirectionEnum.WEST);
+        buttons.appendChild(leftButton);
+
+        const middleButtons = document.createElement('div');
+        middleButtons.classList.add('middle-buttons');
+
+        const upButton = document.createElement('button');
+        upButton.textContent = 'North';
+        upButton.onclick = () => this.goto(DirectionEnum.NORTH);
+        middleButtons.appendChild(upButton);
+
+        const downButton = document.createElement('button');
+        downButton.textContent = 'South';
+        downButton.onclick = () => this.goto(DirectionEnum.SOUTH);
+        middleButtons.appendChild(downButton);
+
+        buttons.appendChild(middleButtons);
+
+        const rightButton = document.createElement('button');
+        rightButton.textContent = 'Est';
+        rightButton.onclick = () => this.goto(DirectionEnum.EST);
+        buttons.appendChild(rightButton);
+
+        toolbar.appendChild(buttons);
+        container.appendChild(toolbar);
+
         this.canvas = document.createElement('canvas');
         this.canvas.classList.add('draw-zone');
         this.canvasCtx = this.canvas.getContext('2d');
@@ -52,7 +163,7 @@ export class OceanOfCodeComponent extends HTMLElement {
         )
     }
 
-    private draw(path?: number[]) {
+    private draw() {
         this.clear();
         this.drawGrid();
     }
@@ -62,23 +173,74 @@ export class OceanOfCodeComponent extends HTMLElement {
     }
 
     private drawGrid() {
-        const marge = 6;
-        this.canvasCtx.beginPath();
+        // DRAW OCEAN
+        this.canvasCtx.fillStyle = this.SEA_COLOR;
+        this.canvasCtx.fillRect(
+            OceanOfCodeComponent.MARGE,
+            OceanOfCodeComponent.MARGE,
+            this.cellSize * this.grid.width,
+            this.cellSize * this.grid.height);
 
         // DRAW LINE
+        this.canvasCtx.beginPath();
         this.canvasCtx.strokeStyle = this.LINE_COLOR;
-        this.canvasCtx.lineWidth = 2;
-        for (let x = 0; x <= this.grid.width; x++) {
-            this.canvasCtx.moveTo(x * this.cellSize + marge, 0 + marge);
-            this.canvasCtx.lineTo(x * this.cellSize + marge, this.cellSize * this.grid.height + marge)
-        }
 
-        for (let y = 0; y <= this.grid.width; y++) {
-            this.canvasCtx.moveTo(0 + marge, y * this.cellSize + marge);
-            this.canvasCtx.lineTo(this.cellSize * this.grid.width + marge, y * this.cellSize + marge)
-        }
+        for (let i = 0; i <= this.grid.width; i++) {
+            if (i % 5 !== 0) {
+                this.canvasCtx.moveTo(i * this.cellSize + OceanOfCodeComponent.MARGE, OceanOfCodeComponent.MARGE);
+                this.canvasCtx.lineTo(i * this.cellSize + OceanOfCodeComponent.MARGE, this.cellSize * this.grid.height + OceanOfCodeComponent.MARGE);
 
-        this.canvasCtx.closePath();
+                this.canvasCtx.moveTo(OceanOfCodeComponent.MARGE, i * this.cellSize + OceanOfCodeComponent.MARGE);
+                this.canvasCtx.lineTo(this.cellSize * this.grid.width + OceanOfCodeComponent.MARGE, i * this.cellSize + OceanOfCodeComponent.MARGE);
+
+            }
+        }
         this.canvasCtx.stroke();
+        this.canvasCtx.closePath();
+
+        // DRAW SURFACE LINE
+        this.canvasCtx.lineWidth = 2;
+        this.canvasCtx.beginPath();
+        this.canvasCtx.strokeStyle = this.SURFACE_COLOR;
+        for (let i = 0; i <= this.grid.width; i++) {
+            if (i % 5 === 0) {
+                this.canvasCtx.moveTo(i * this.cellSize + OceanOfCodeComponent.MARGE, OceanOfCodeComponent.MARGE);
+                this.canvasCtx.lineTo(i * this.cellSize + OceanOfCodeComponent.MARGE, this.cellSize * this.grid.height + OceanOfCodeComponent.MARGE);
+
+                this.canvasCtx.moveTo(OceanOfCodeComponent.MARGE, i * this.cellSize + OceanOfCodeComponent.MARGE);
+                this.canvasCtx.lineTo(this.cellSize * this.grid.width + OceanOfCodeComponent.MARGE, i * this.cellSize + OceanOfCodeComponent.MARGE);
+            }
+        }
+        this.canvasCtx.stroke();
+        this.canvasCtx.closePath();
+
+        // DRAW ISLAND
+        this.grid.cells
+            .filter(cell => cell.type === CellTypeEnum.ISLAND)
+            .forEach(cell => this.drawIsland(cell));
+
+        this.stats.forEach(stat => this.drawText(stat));
+    }
+
+    private drawText({stat, cell}: { stat: number, cell: Cell }) {
+        if (cell) {
+            this.canvasCtx.font = "1em Arial";
+            this.canvasCtx.fillStyle = this.TEXT_COLOR;
+            this.canvasCtx.textAlign = "center";
+            this.canvasCtx.textBaseline = 'middle';
+            this.canvasCtx.fillText(stat.toString(),
+                OceanOfCodeComponent.MARGE + cell.x * this.cellSize + this.cellSize / 2,
+                OceanOfCodeComponent.MARGE + cell.y * this.cellSize + this.cellSize / 2,
+                this.cellSize);
+        }
+    }
+
+    private drawIsland(cell: Cell) {
+        this.canvasCtx.fillStyle = this.ISLAND_COLOR;
+        this.canvasCtx.fillRect(
+            OceanOfCodeComponent.MARGE + cell.x * this.cellSize,
+            OceanOfCodeComponent.MARGE + cell.y * this.cellSize,
+            this.cellSize,
+            this.cellSize);
     }
 }
